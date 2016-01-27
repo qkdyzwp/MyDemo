5091cdaea698325c39b96a95991851b0b7ce7067
qkdyzwp <446893043@qq.com>
2016-01-27 08:40:49 +0800

    增加人员

---
+++ app/src/main/java/com/fir/wp/mydemo/bean/person.java
@@ -0,0 +1,35 @@
+package com.fir.wp.mydemo.bean;
+
+/**
+ * Created by wp on 2016/1/27.
+ * about
+ */
+public class person {
+    private String name;
+    private String age;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
